148b9784768043bdea4096a96ffb9d57a47d0a18
Matevz <stinjek@gmail.com>
2019-05-17 00:24:57 +0200

    cleanup

--- client/src/components/records_list/index.js
+++
@@ -1,3 +0,0 @@
-export const RecordsListHeader = require('./RecordsListHeader')
-export const RecordsListRow = require('./RecordsListRow')
-export const RecordsListFooter = require('./RecordsListFooter')
