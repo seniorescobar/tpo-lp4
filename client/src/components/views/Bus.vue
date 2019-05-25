a12802ad999aac54057a1f8d29482f7d9f4ecbc1
Matevz <stinjek@gmail.com>
2019-05-25 20:34:49 +0200

    events example

--- client/src/components/views/Bus.vue
+++ client/src/components/views/Bus.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
+    <div class="container">
         <iframe class="iframe" src="https://www.trola.si/" />
+        <div class="loading">Loading</div>
     </div>
 </template>
 
@@ -13,10 +14,21 @@ export default {
 <style lang="less" scoped>
 @import '../../less/common';
 
+.container {
+    position: relative;
+}
 .iframe {
     display: block;
     margin: 64px auto 0;
     width: 600px;
     height: 500px;
+}
+.loading {
+    position: absolute;
+    margin: 64px auto;
+    top: 64px;
+    left: 0;
+    right: 0;
+    text-align: center;
 }
 </style>
--- client/src/components/views/Food.vue
+++ client/src/components/views/Food.vue
@@ -1,14 +1,26 @@
 <template>
-    <div>
-        <iframe class="maps" src="https://maps.google.com/maps?q=ljubljana%20restaurants&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    <div class="container">
+        <iframe class="iframe" src="https://maps.google.com/maps?q=ljubljana%20restaurants&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+        <div class="loading">Loading</div>
     </div>
 </template>
 
 <style lang="less" scoped>
-.maps {
+.container {
+    position: relative;
+}
+.iframe {
     display: block;
     margin: 64px auto 0;
     width: 600px;
     height: 500px;
+}
+.loading {
+    position: absolute;
+    margin: 64px auto;
+    top: 64px;
+    left: 0;
+    right: 0;
+    text-align: center;
 }
 </style>
